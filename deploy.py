339f8955ef2a9a1b0c123b3411592091bcb039f2
Maximum21 <bcs02133361@gmail.com>
2022-07-29 18:14:56 +0100

    Add files via upload

--- deploy.py
+++ deploy.py
@@ -229,6 +229,7 @@ if __name__ == '__main__':
 
     # Upload app file and get shared url
     file_url = upload_to_dropbox(target_app_file, app_file, options.dropbox_token, options.dropbox_folder)
+print(file_url)
     if file_url == None:
         exit(DROPBOX_ERROR_CODE)
     
